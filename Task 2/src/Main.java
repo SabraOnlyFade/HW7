11349d03827a9b2060322cd36dd3377bd0f47570
GAME ON DP <evasabra4@gmail.com>
2023-06-27 00:38:40 +0300

    Small programs

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
+        int id = 2;
+        while (id<=100) {
+            if(id % 2 == 0)
+            {System.out.println("Уволен айтишник с id "+ id);
+                id = id + 2;
+            }
+        }
+        }
     }
-}
